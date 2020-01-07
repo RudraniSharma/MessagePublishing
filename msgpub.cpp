76e88e8466508c7ddf42fa379888c03ed7965f56
Rudrani Sharma <rudranisharma@gmail.com>
2020-01-07 23:53:15 +0100

    new structure
    new

--- msgpub.cpp
+++ msgpub.cpp
@@ -1,24 +1,24 @@
 
-
-
+#include <stdio.h>
 
 //device
-
+void device_reciever()
 {
-   
+
 }
 
 
 
 // host
-
+ void host_publish()
 {
-   
+
 }
 
 
-
+// main
 int main()
 {
-    
+	printf("Hello World");
+	return 0;
 }
