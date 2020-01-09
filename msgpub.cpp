8a804d040005dd9cafa96f8d10b1089ef8b461fc
Rudrani Sharma <rudranisharma@gmail.com>
2020-01-09 15:18:42 +0100

    New
    Pub Sub Messaging CUDA

--- msgpub.cpp
+++ msgpub.cpp
@@ -8,43 +8,43 @@
 class Unified {
 public:
   void *operator new(size_t len) {
-    void *ptr;
-    cudaMallocManaged(&ptr, len);
-    return ptr;
+	void *ptr;
+	cudaMallocManaged(&ptr, len);
+	return ptr;
   }
   void operator delete(void *ptr) {
-    cudaFree(ptr);
+	cudaFree(ptr);
   }
 
 
   void* operator new[] (std::size_t size) {
-    void *ptr;
-    cudaMallocManaged(&ptr,size);
-    return ptr;
+	void *ptr; 
+	cudaMallocManaged(&ptr,size);
+	return ptr;
   }
   void operator delete[] (void* ptr) {
-    cudaFree(ptr);
+	cudaFree(ptr);
   }
 };
 
 
-class publisher : public Unified
+class publisher : public Unified 
 {
 public:
-    float value;
-    __device__ void setValue(float v) { value=v; }
+	float value;
+	__device__ void setValue(float v) { value=v; }
 };
 
 __global__ void publish_msg(publisher *topic,float num) {
-    int i=threadIdx.x + blockIdx.x*blockDim.x;
-    topic[i].setValue(num);
+	int i=threadIdx.x + blockIdx.x*blockDim.x; 
+	topic[i].setValue(num);
 }
 
-class subscriber : public Unified
+class subscriber : public Unified 
 {
 public:
-    float value;
-    __device__ void getValue(float v) { value=v; }
+	float value;
+	__device__ void getValue(float v) { value=v; }
 };
 
 /* GPU kernel: set an array of topic to a value */
@@ -53,32 +53,26 @@ __host__ void sub_msg(publisher *topic,int i) {
 }
 
 
-int main(int argc,char *argv[])
+int main(int argc,char *argv[]) 
 {
 
-        int n=1;
+        int n=1; 
         int i=0;
-        publisher *topic=new publisher[n];
-    worker<<<1,1>>>(topic);
-    //subscribe(topic_out);
-    publish_msg(topic, 6.9);  // gpu kernel recv msg, replies to topic
-    m = recv(topic);  // blocking
-    // m== reply from gpu
-    
+        publisher *topic=new publisher[n]; 
         publish_msg<<<1,1>>>(topic,6.9); /* GPU */
-    //std::cout<<"Topic["<<i<<"] = "<<topic[i].value<<"\n";
-    cudaDeviceSynchronize();
-    sub_msg(topic,i);
+	//std::cout<<"Topic["<<i<<"] = "<<topic[i].value<<"\n";
+	cudaDeviceSynchronize();
+	sub_msg(topic,i);
 
        
 
-    i++;
-    publisher *topic1=new publisher[n];
-    publish_msg<<<1,2>>>(topic1,7.7);
-    //std::cout<<"Topic["<<i<<"] = "<<topic1[i].value<<"\n";
-    cudaDeviceSynchronize();
-           sub_msg(topic1,i);
+	i++;
+	publisher *topic1=new publisher[n];
+	publish_msg<<<1,2>>>(topic1,7.7);
+	//std::cout<<"Topic["<<i<<"] = "<<topic1[i].value<<"\n";
+	cudaDeviceSynchronize();
+       	//sub_msg(topic1,i);
 
-    return 0;
+	return 0;
 
 }
